cc9b5ef3ca1aa12bc1ff51e56da8e8e400bf5abd
jurgz42 <jurgentisza@gmail.com>
2023-04-18 18:44:16 -0500

    Update divide.sv

--- divide/divide.sv
+++ divide/divide.sv
@@ -16,7 +16,7 @@ module divide #(
 logic signed [DATA_WIDTH-1:0] Q, Q_c; //Q is dividend
 logic signed [DATA_WIDTH-1:0] B, B_c; //B is divisor
 logic signed [2*DATA_WIDTH:0] EAQ, EAQ_c;
-int i_c, i;
+logic[7:0] i_c, i;
 
 enum logic[1:0] {s0, s1, s2} state, next_state;
 
@@ -55,8 +55,8 @@ always_comb begin
 
         s1: begin
             EAQ_c = EAQ << 1;
-            if(EAQ_c[2*DATA_WIDTH]) EAQ_c[2*DATA_WIDTH:DATA_WIDTH] = EAQ_c[DATA_WIDTH*2-1:DATA_WIDTH] + B;
-            else                    EAQ_c[2*DATA_WIDTH:DATA_WIDTH] = EAQ_c[DATA_WIDTH*2-1:DATA_WIDTH] - B;
+            if(EAQ_c[2*DATA_WIDTH]) EAQ_c[2*DATA_WIDTH:DATA_WIDTH] = EAQ_c[DATA_WIDTH*2-1:DATA_WIDTH] + (DATA_WIDTH+1)'(B);
+            else                    EAQ_c[2*DATA_WIDTH:DATA_WIDTH] = EAQ_c[DATA_WIDTH*2-1:DATA_WIDTH] - (DATA_WIDTH+1)'(B);
 
             if(EAQ_c[2*DATA_WIDTH]) EAQ_c[0] = 1'b0;
             else                    EAQ_c[0] = 1'b1;
