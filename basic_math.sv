ed9ee0982c72670ca375729710ae78575fe88287
jurgz42 <jurgentisza@gmail.com>
2023-04-19 14:53:16 -0500

    typos

--- basic_math.sv
+++ basic_math.sv
@@ -42,15 +42,15 @@ module scale #(
     parameter Q_BITS = 'd10
 ) (
     input int x[2:0],
-    input int y[2:0],
+    input int a,
     output int out[2:0]
 );
 	logic signed [47:0] out_big [2:0];
 	
     always_comb begin
-        out_big[0] = (x[0] * a) >> 16;
-        out_big[1] = (x[1] * a) >> 16;
-        out_big[2] = (x[2] * a) >> 16;
+        out_big[0] = (x[0] * a) >> Q_BITS;
+        out_big[1] = (x[1] * a) >> Q_BITS;
+        out_big[2] = (x[2] * a) >> Q_BITS;
 
         out[0] = out0_big;
         out[1] = out1_big;
