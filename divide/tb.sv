4d0c89caa6347de2ad7c521a3f81a50c639fc906
jurgz42 <jurgentisza@gmail.com>
2023-04-18 23:33:11 -0500

    Update tb.sv

--- divide/tb.sv
+++ divide/tb.sv
@@ -49,8 +49,8 @@ end
 
 initial begin: input_process
     @(negedge clock);
-    dividend =  'd190 << QUANTIZED_BITS;
-    divisor =   'd7 << QUANTIZED_BITS;
+    dividend =  -'d190 << QUANTIZED_BITS;
+    divisor =   -'d7 << QUANTIZED_BITS;
     valid_in = 'b1;
 end
 
