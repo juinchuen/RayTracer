68ddacc7d09e010540f3ebad008ae7bf12a5071d
jurgz42 <jurgentisza@gmail.com>
2023-04-18 23:36:56 -0500

    Update divide.sv

--- divide/divide.sv
+++ divide/divide.sv
@@ -38,6 +38,8 @@ end
 
 logic signed [ED_WIDTH-1:0] temp;
 
+logic signed [D_WIDTH-1:0] temp_divisor, temp_dividend;
+
 always_comb begin
     next_state = state;
     quotient = 'b0;
@@ -52,13 +54,15 @@ always_comb begin
                 //if one is negative
                 if(dividend < 0) begin
                     dividend_flag = 'b1;
+                    temp_dividend = -dividend;
                 end
                 if(divisor < 0) begin
                     divisor_flag = 'b1;
+                    temp_divisor = -divisor;
                 end
 
-                B_c = divisor;
-                temp = ((ED_WIDTH)'(dividend) << Q_BITS) + (divisor >> 1);
+                B_c = temp_divisor;
+                temp = ((ED_WIDTH)'(temp_dividend) << Q_BITS) + (temp_divisor >> 1);
                 EAQ_c = {1'b0, ED_WIDTH'(0), temp};
                 i_c = 'b0;
                 next_state = s1;
@@ -80,7 +84,11 @@ always_comb begin
         end
 
         s2: begin   //write
-            quotient = EAQ[D_WIDTH-1:0]; //quotient in Q     EAQ[ED_WIDTH-1:0]
+            if(dividend_flag^divisor_flag)
+                quotient = -EAQ[D_WIDTH-1:0]; //quotient in Q     EAQ[ED_WIDTH-1:0]
+            else
+                quotient = EAQ[D_WIDTH-1:0]; //quotient in Q     EAQ[ED_WIDTH-1:0]
+            
             valid_out = 'b1;
         end
     endcase
