1a85aa1e8443300aeff8bb7c492e96107c445b2e
jurgz42 <jurgentisza@gmail.com>
2023-04-18 23:13:17 -0500

    Expanded width test

--- divide/divide.sv
+++ divide/divide.sv
@@ -1,21 +1,21 @@
 module divide #(
-    parameter QUANTIZED_BITS = 'd10,
-    parameter DATA_WIDTH = 'd32
+    parameter Q_BITS = 'd10,
+    parameter D_WIDTH = 'd32,       //data width
+    parameter ED_WIDTH = 2*D_WIDTH  //expanded data width
 )(
     input logic clock,
     input logic reset,
-    input logic signed [DATA_WIDTH-1:0] dividend,
-    input logic signed [DATA_WIDTH-1:0] divisor,
-    output logic signed [DATA_WIDTH-1:0] quotient,
-    output logic signed [DATA_WIDTH-1:0] remainder,
+    input logic signed [D_WIDTH-1:0] dividend,
+    input logic signed [D_WIDTH-1:0] divisor,
+    output logic signed [D_WIDTH-1:0] quotient,
 
     input logic valid_in,
     output logic valid_out
 );
 
-logic signed [DATA_WIDTH-1:0] Q, Q_c; //Q is dividend
-logic signed [DATA_WIDTH-1:0] B, B_c; //B is divisor
-logic signed [2*DATA_WIDTH:0] EAQ, EAQ_c;
+logic signed [D_WIDTH-1:0] Q, Q_c; //Q is dividend
+logic signed [D_WIDTH-1:0] B, B_c; //B is divisor
+logic signed [2*ED_WIDTH:0] EAQ, EAQ_c; //register with all our math
 logic[7:0] i_c, i;
 
 enum logic[1:0] {s0, s1, s2} state, next_state;
@@ -34,7 +34,7 @@ always_ff @(posedge clock or posedge reset) begin
     end
 end
 
-logic [2*DATA_WIDTH-1:0] temp;
+logic signed [ED_WIDTH-1:0] temp;
 
 always_comb begin
     next_state = state;
@@ -49,8 +49,8 @@ always_comb begin
         s0: begin
             if(valid_in) begin
                 B_c = divisor;
-                temp = ((2*DATA_WIDTH)'(dividend) << QUANTIZED_BITS) + (divisor >> 1);
-                EAQ_c = {1'b0, DATA_WIDTH'(0), DATA_WIDTH'(temp)};
+                temp = ((EXPAND_DATA_WIDTH)'(dividend) << Q_BITS) + (divisor >> 1);
+                EAQ_c = {1'b0, EXPAND_DATA_WIDTH'(0), temp};
                 i_c = 'b0;
                 next_state = s1;
             end
@@ -58,21 +58,20 @@ always_comb begin
 
         s1: begin
             EAQ_c = EAQ << 1;
-            if(EAQ_c[2*DATA_WIDTH]) EAQ_c[2*DATA_WIDTH:DATA_WIDTH] = EAQ_c[2*DATA_WIDTH:DATA_WIDTH] + (DATA_WIDTH+1)'(B);
-            else                    EAQ_c[2*DATA_WIDTH:DATA_WIDTH] = EAQ_c[2*DATA_WIDTH:DATA_WIDTH] - (DATA_WIDTH+1)'(B);
+            if(EAQ_c[2*ED_WIDTH])   EAQ_c[2*ED_WIDTH:ED_WIDTH] = EAQ_c[2*ED_WIDTH:ED_WIDTH] + (ED_WIDTH+1)'(B);
+            else                    EAQ_c[2*ED_WIDTH:ED_WIDTH] = EAQ_c[2*ED_WIDTH:ED_WIDTH] - (ED_WIDTH+1)'(B);
 
-            if(EAQ_c[2*DATA_WIDTH]) EAQ_c[0] = 1'b0;
+            if(EAQ_c[2*ED_WIDTH])    EAQ_c[0] = 1'b0;
             else                    EAQ_c[0] = 1'b1;
             i_c = i + 1;
-            if(i_c == DATA_WIDTH) begin
-                if(EAQ_c[2*DATA_WIDTH]) EAQ_c[2*DATA_WIDTH:DATA_WIDTH] = EAQ_c[2*DATA_WIDTH:DATA_WIDTH] + B;
+            if(i_c == ED_WIDTH) begin
+                if(EAQ_c[2*ED_WIDTH]) EAQ_c[2*ED_WIDTH:ED_WIDTH] = EAQ_c[2*ED_WIDTH:ED_WIDTH] + B;
                 next_state = s2;
             end
         end
 
         s2: begin   //write
-            quotient = EAQ[DATA_WIDTH-1:0]; //quotient in Q     EAQ[DATA_WIDTH-1:0]
-            remainder = EAQ[DATA_WIDTH*2-1:DATA_WIDTH]; //remainder in A    EAQ[DATA_WIDTH*2-1:DATA_WIDTH]
+            quotient = EAQ[D_WIDTH-1:0]; //quotient in Q     EAQ[ED_WIDTH-1:0]
             valid_out = 'b1;
         end
     endcase
