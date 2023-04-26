856bf8c906652f1cce7f01f2e7eac95718bb2093
jurgz42 <jurgentisza@gmail.com>
2023-04-26 18:52:07 -0500

    Update scale.sv

--- sv/fifo_math/scale/scale.sv
+++ sv/fifo_math/scale/scale.sv
@@ -92,7 +92,7 @@ scale_module u_scale_module (
     .clock        (clock),
     .reset        (reset),
     .x            (x[2:0]),
-    .y            (y[2:0]),
+    .y            (a),
     .in_empty     (in_empty),
     .in_rd_en     (in_rd_en),
     .out          (out_din[2:0]),
